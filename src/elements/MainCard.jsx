fde28c7522e7caaeee99d9d26a06c75057085f53
CaiooFerreiaa <caioferreiraadev@gmail.com>
2025-07-23 02:37:34 -0300

    Update: Atualizando caminho da imagem

--- src/elements/MainCard.jsx
+++ src/elements/MainCard.jsx
@@ -5,7 +5,7 @@ export default function MainCard({ pokemonForCatch }) {
   return (
     <div id="main-card">
       <div className="img-container">
-        <img src="/src/assets/avatar.png" alt="Aqui é a imagem do personagem" id="img-trainner"/>
+        <img src="/avatar.png" alt="Aqui é a imagem do personagem" id="img-trainner"/>
       </div>
       <CardPokemon pokemonForCatch={pokemonForCatch}/>
     </div>
