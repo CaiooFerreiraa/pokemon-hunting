311a97dba1f4e8902a1e15b239df6f3641c1ca28
CaiooFerreiaa <caioferreiraadev@gmail.com>
2025-09-17 17:20:21 -0300

    Finale: Refatorando codigo python

--- backend/CapturePokemon.py
+++ backend/CapturePokemon.py
@@ -4,23 +4,25 @@ from flask import jsonify
 from PIL import Image
 
 class ActionsForCapturePokemon:
-  namePokemonBeingHunted = None
-  pokemonSuccessfulyCaugth = None
-  conditionLoop = False
-  useMove = "falseswipe"
-  crop_area = getCoordenadas.loadCoords()
-  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-  pytesseract.pytesseract.tesseract_cmd = os.path.join(BASE_DIR, "Tesseract-OCR", "tesseract.exe")
+  def __init__(self):
+        self.namePokemonBeingHunted = None
+        self.pokemonSuccessfulyCaugth = None
+        self.conditionLoop = False
+        self.useMove = "falseswipe"
+        self.crop_area = getCoordenadas.loadCoords()
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        pytesseract.pytesseract.tesseract_cmd = os.path.join(self.BASE_DIR, "Tesseract-OCR", "tesseract.exe")
 
-  def loopCapture():
-    if ActionsForCapturePokemon.crop_area is None:
+
+  def loopCapture(self):
+    if self.crop_area is None:
         return
     
-    while ActionsForCapturePokemon.conditionLoop:
-        ActionsForCapturePokemon.botMovement()
-        if ActionsForCapturePokemon.namePokemonBeingHunted in ActionsForCapturePokemon.normalizedText():
+    while self.conditionLoop:
+        self.botMovement()
+        if self.namePokemonBeingHunted in self.normalizedText():
             pyautogui.press('1')
-            ActionsForCapturePokemon.useFalseSwipe()
+            self.useFalseSwipe()
         else:
             pyautogui.press('4')
         time.sleep(0.2)
@@ -31,12 +33,12 @@ class ActionsForCapturePokemon:
       time.sleep(1)
       pyautogui.keyDown('a')
 
-  def normalizedText():
-    image = ActionsForCapturePokemon.screenshot_region(ActionsForCapturePokemon.crop_area)
+  def normalizedText(self):
+    image = self.screenshot_region(self.crop_area)
     text = pytesseract.image_to_string(image)
     return text.lower().replace(" ", "")
   
-  def screenshot_region(crop_area):
+  def screenshot_region(self, crop_area):
     if crop_area is None:
         raise ValueError("crop_area não definido! Defina a área antes de iniciar a captura.")
 
@@ -45,17 +47,17 @@ class ActionsForCapturePokemon:
         imageScreen = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
         return imageScreen.crop(crop_area)
 
-  def useFalseSwipe():
+  def useFalseSwipe(self):
     pyautogui.press('1')
     time.sleep(3)
-    if ActionsForCapturePokemon.useMove in ActionsForCapturePokemon.normalizedText():
+    if self.useMove in self.normalizedText():
       pyautogui.press('3')
-      ActionsForCapturePokemon.catchPokemon()
+      self.catchPokemon()
 
-  def catchPokemon():
+  def catchPokemon(self):
     while True:
         pyautogui.press('3')
-        if ActionsForCapturePokemon.pokemonSuccessfulyCaugth in ActionsForCapturePokemon.normalizedText():
+        if self.pokemonSuccessfulyCaugth in self.normalizedText():
             break
         else:
             pyautogui.press('1')
@@ -64,18 +66,18 @@ class ActionsForCapturePokemon:
   def getCropArea():
     return getCoordenadas.loadCoords()
   
-  def getPokemonHunted():
-    return ActionsForCapturePokemon.namePokemonBeingHunted
+  def getPokemonHunted(self):
+    return self.namePokemonBeingHunted
 
-  def setNamePokemonBeighHunted(name):
-    ActionsForCapturePokemon.namePokemonBeingHunted = name
+  def setNamePokemonBeighHunted(self, name):
+    self.namePokemonBeingHunted = name
 
-  def setPokemonSuccessfulyCaugth(msgPokemon):
-    ActionsForCapturePokemon.pokemonSuccessfulyCaugth = f"success!youcaught{msgPokemon.lower()}"
+  def setPokemonSuccessfulyCaugth(self, msgPokemon):
+    self.pokemonSuccessfulyCaugth = f"success!youcaught{msgPokemon.lower()}"
   
-  def setConditionLoop(condition):
-    ActionsForCapturePokemon.conditionLoop = condition
+  def setConditionLoop(self, condition):
+    self.conditionLoop = condition
 
-  def checkCropAreaExists():
-    if ActionsForCapturePokemon.getCropArea() is None:
+  def checkCropAreaExists(self):
+    if self.getCropArea() is None:
       return jsonify({"status": "Erro", "message": "Defina a área primeiro!"})
